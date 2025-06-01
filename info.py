4af139fb27289c6b5dc3f26128a9f187f1a9d8d9
hide-sudo <deathhide08@gmail.com>
2025-06-01 13:58:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ API_HASH = environ.get('API_HASH', '7d2c9a5628814e1430b30a1f0dc0165b')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7859288880:AAE8k3pJNNJCkxwJF0loVHgpLp8psnpHia4')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-USERNAME = environ.get('USERNAME', "https://telegram.me/Mr_Pbail")
+USERNAME = environ.get('USERNAME', "https://telegram.me/Mr_deva")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002525011494'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/PbailMovieRequestGroup')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/NIshumoviesRequestGroup')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002545071865').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://coderx0805:EjFHGDShcPT7J2hM@cluster0.bm8jhbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
